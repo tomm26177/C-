4702dc6e89d1ed5220f7f350e81b01932e9493d1
Tomasz Kamiński <104736559+tomm26177@users.noreply.github.com>
2023-03-26 11:58:03 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+  {
+    static void Main(string [] args){
+      Console.Write("heloo");
+      Console.Read();
+    }
+  }
+
+  
+
+
+  
+
